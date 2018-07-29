91fb20351677097244cd8f092a25943cc846718a
finninde <finninde@stud.ntnu.no>
2018-07-29 18:07:00 +0200

    Add geometries

--- src/SToneInc.js
+++ src/SToneInc.js
@@ -34,22 +34,71 @@
       this.ctx.clearRect(0, 0, 16 * GU, 9 * GU);
 
       this.circleEndSize = smoothstep(0, 1, (this.frame - 827) / (996 - 827));
-
-      var colors = ["#729C34", "#265B6A"];
-
-      this.ctx.fillStyle = colors[0];
-      this.ctx.strokeStyle = colors[0];
+      
+      var colors = {
+                    orange: "#DC6837", 
+                    red: "#8F343B", 
+                    white: "#F4F1E8"};
+      
+      // Das Background      
+      const skyline = 3.5; 
+      this.ctx.fillStyle = colors.white;
+      this.ctx.fillRect(0,0,16*GU, skyline*GU);
+      this.ctx.fillStyle = colors.orange;
+      this.ctx.fillRect(0, skyline*GU, 16*GU, (9-skyline)*GU);
+      
+      
+      // Ball shadow
+      const ballRad = 1.5;
+      this.ctx.fillStyle = colors.white;
+      this.ctx.fillRect(4*GU, (skyline)*GU, 16*GU, 2*ballRad*GU);
+      
+      // Ball, the one and only the rest are shoes ofc
+      this.ctx.strokeStyle = colors.red;
+      this.ctx.fillStyle = colors.red;
       this.ctx.beginPath();
       this.ctx.ellipse(
-        2 * GU,
-        2 * GU,
-        2 * GU,
-        2 * GU,
+        4 * GU,
+        (skyline + ballRad) * GU,
+        ballRad * GU,
+        ballRad * GU,
         1, 0, Math.PI * 2);
       this.ctx.lineWidth = 0.5 * GU ;
-      this.ctx.stroke();
+      //this.ctx.stroke();
       this.ctx.fill();
 
+      // Top Shoe
+      this.ctx.fillStyle = colors.red;
+      this.ctx.fillRect(0, (skyline - ballRad)*GU, (16-ballRad*0.9)*GU, ballRad*GU);
+      this.ctx.strokeStyle = colors.red;
+      this.ctx.beginPath();
+      this.ctx.ellipse(
+        (16 - ballRad*0.9) * GU,
+        (skyline - ballRad) * GU,
+        ballRad * GU,
+        ballRad * GU,
+        1, 0, Math.PI * 2);
+      this.ctx.lineWidth = 0.5 * GU ;
+      //this.ctx.stroke();
+      this.ctx.fill();
+
+      // Bottom Shoe
+      this.ctx.fillStyle = colors.red; 
+      this.ctx.fillRect = colors.red;
+  
+      this.ctx.fillStyle = colors.orange;
+      this.ctx.fillRect(0, (skyline - ballRad)*GU, (16-ballRad*0.9)*GU, ballRad*GU);
+      this.ctx.strokeStyle = colors.red;
+      this.ctx.beginPath();
+      this.ctx.ellipse(
+        (16 - ballRad*0.9) * GU,
+        (skyline - ballRad) * GU,
+        ballRad * GU,
+        ballRad * GU,
+        1, 0, Math.PI * 2);
+      this.ctx.lineWidth = 0.5 * GU ;
+      //this.ctx.stroke();
+      this.ctx.fill();
       this.output.needsUpdate = true;
       this.outputs.render.setValue(this.output);
     }
